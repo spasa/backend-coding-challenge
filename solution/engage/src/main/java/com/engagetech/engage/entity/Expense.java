9e67260b198cd84f7ba6af81562e869dd4c6358b
Spasa Mihajlovic <msvsbc@yahoo.com>
2018-01-30 00:50:38 +0100

    add expense entity class

---
+++ solution/engage/src/main/java/com/engagetech/engage/entity/Expense.java
@@ -0,0 +1,86 @@
+package com.engagetech.engage.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.math.BigDecimal;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Expense {
+    
+    private Long id;
+    private Long userId;
+    private Long sessionId;
+    private BigDecimal amount;
+    private BigDecimal taxValue;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(Long sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getTaxValue() {
+        return taxValue;
+    }
+
+    public void setTaxValue(BigDecimal taxValue) {
+        this.taxValue = taxValue;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Expense other = (Expense) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" + "id=" + id + ", userId=" + userId + ", sessionId=" + sessionId + ", amount=" + amount + ", taxValue=" + taxValue + '}';
+    }
+    
+}
